ed68d633a18b88336db312d3561f4ad6505a7a00
Christoph Lassner <Christoph.Lassner@informatik.uni-augsburg.de>
2015-01-02 13:51:05 +0100

    Buildscript-update.

--- build_release.bat
+++ build_release.bat
@@ -10,5 +10,5 @@ start /wait cmd /C "scons --with-serialization --with-python --jobs=10"
 REM Build the rest separately to not have Python dependencies.
 echo Building core library, tests, C++ examples and Matlab interface.
 start /wait cmd /C "scons --with-serialization --with-tests --with-examples --with-matlab --jobs=10"
-echo Zipping.
-"C:\Program Files\7-Zip\7z" a fertilized-package.zip bin CodeGenerator documentation external fertilized fertilized_tests include lib matfertilized Presentation pyfertilized SConsChecks fertilized.sln license.txt README.MD SConscript.py SConstruct version.py
+REM echo Zipping.
+REM "C:\Program Files\7-Zip\7z" a fertilized-package.zip bin CodeGenerator documentation external fertilized fertilized_tests include lib matfertilized Presentation pyfertilized SConsChecks fertilized.sln license.txt README.MD SConscript.py SConstruct version.py
