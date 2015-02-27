470bc4434b80a7e35f5f81a34992704cf92f7753
Christoph Lassner <mail@christophlassner.de>
2015-02-27 20:01:30 +0100

    Added test template.

---
+++ fertilized_tests/test_template.cpp
@@ -0,0 +1,20 @@
+// Author: ...
+// Leave this as the first import to automatically prevent gcc to
+// emit a warning about boost test on older gcc version.
+#include <fertilized/fertilized.h>
+
+#include "boost/test/unit_test.hpp"
+// If you need the `timeit` function to do time measurements.
+#include "timeit.h"
+// Provides convenience data-creators for tests.
+#include "setup.h"
+
+using namespace fertilized;
+
+BOOST_AUTO_TEST_SUITE(YourTestSuiteName);
+
+BOOST_AUTO_TEST_CASE(YourTest1) {
+    BOOST_REQUIRE(true);
+};
+
+BOOST_AUTO_TEST_SUITE_END();
