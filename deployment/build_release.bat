148da58f3a4f41ed710fcc030f0930e8682aa15b
Christoph Lassner <mail@christophlassner.de>
2015-05-22 02:28:48 +0200

    Absolute paths for OpenCV and deployment adjustment.

--- deployment/build_release.bat
+++ deployment/build_release.bat
@@ -13,4 +13,6 @@ REM Build the rest separately to not have Python dependencies.
 echo Building core library, tests and C++ examples.
 start /wait cmd /C "scons --with-serialization --with-tests --with-examples --with-caffe --caffe-model-dir=C:\caffe-models --temp-folder=c:\tmp --cpu-only --jobs=2"
 echo Zipping.
-"C:\Program Files\7-Zip\7z" a fertilized-package.zip .git bin CodeGenerator deployment documentation external fertilized fertilized_tests IDEs include lib matfertilized Presentation pyfertilized SConsChecks site_scons license.txt README.MD SConscript.py SConstruct setup_paths.bat compile.bat setup_environment.bat setup_environment_windows.py version.py
+copy deployment\README.MD README-BIN.MD
+"C:\Program Files\7-Zip\7z" a fertilized-package.zip bin documentation fertilized_tests include lib Presentation pyfertilized license.txt README-BIN.MD
+del README-BIN.MD
