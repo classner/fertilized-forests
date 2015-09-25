db617bee3365f14806e162960b8b330696c16041
Christoph Lassner <mail@christophlassner.de>
2015-09-25 15:18:42 +0200

    Updated .travis.yml.

--- build_support/travis/setup_python.sh
+++ build_support/travis/setup_python.sh
@@ -1,5 +1,5 @@
 
-if [ "${BUILD_WITH_SERIALIZATION_WO_PYTHON}" = "2" ]; then
+if [ "${PYTHON_VERSION}" = "2" ]; then
   wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
   chmod +x miniconda.sh
   ./miniconda.sh -b
@@ -7,7 +7,7 @@ if [ "${BUILD_WITH_SERIALIZATION_WO_PYTHON}" = "2" ]; then
   export PYTHON_INCLUDE_DIR=/home/travis/miniconda/include/python3.4m
   export PYTHON_LIBRARY=/home/travis/miniconda/lib/libpython3.so
 fi
-if [ "${BUILD_WITH_SERIALIZATION_WO_PYTHON}" = "3" ]; then
+if [ "${PYTHON_VERSION}" = "3" ]; then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
   chmod +x miniconda.sh
   ./miniconda.sh -b
@@ -15,8 +15,9 @@ if [ "${BUILD_WITH_SERIALIZATION_WO_PYTHON}" = "3" ]; then
   export PYTHON_INCLUDE_DIR=/home/travis/miniconda/include/python2.7
   export PYTHON_LIBRARY=/home/travis/miniconda/lib/libpython2.7.so
 fi
-if [ ! "${BUILD_WITH_SERIALIZATION_WO_PYTHON}" = "none" ]; then
+if [ ! "${PYTHON_VERSION}" = "none" ]; then
   export MINICONDA_BIN=/home/travis/miniconda/bin
   sudo /home/travis/miniconda/bin/conda update --yes --quiet conda
+  sudo /home/travis/miniconda/bin/conda install --yes --quiet numpy scipy pillow scikit-image matplotlib scikit-learn
   export PATH=$MINICONDA_BIN:$PATH
 fi
