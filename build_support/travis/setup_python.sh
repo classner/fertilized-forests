acffde0139ff2c0aed5ab890bdf408f13286e39f
Christoph Lassner <mail@christophlassner.de>
2015-09-25 19:50:07 +0200

    Updated apt usage.

--- build_support/travis/setup_python.sh
+++ build_support/travis/setup_python.sh
@@ -9,13 +9,14 @@ if [ "${PYTHON_VERSION}" = "2" ]; then
   # export MINICONDA_BIN=/home/travis/miniconda/bin
   sudo apt-get -y install python python-dev python-numpy python-sklearn
   export PYTHON_INCLUDE_DIR=/usr/include/python2.7
-  export PYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython2.7.so
+  export PYTHON_LIBRARY=/usr/lib/libpython2.7.so
   export PYTHON_EXECUTABLE=/usr/bin/python
 fi
 if [ "${PYTHON_VERSION}" = "3" ]; then
   # wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
   # chmod +x miniconda.sh
   # ./miniconda.sh -b
+  sudo find / -name "libboost_python*.so" 2>/dev/null
   sudo ln -s libboost_python-py34.so /usr/lib/x86_64-linux-gnu/libboost_python3.so
   # export PYTHONPATH=/home/travis/miniconda3/lib/python3.4/site-packages
   # export PYTHON_INCLUDE_DIR=/home/travis/miniconda3/include/python3.4m
