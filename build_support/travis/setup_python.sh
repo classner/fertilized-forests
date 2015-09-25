4e4e620e981488b8f9d053a581cee467b7c15838
Christoph Lassner <mail@christophlassner.de>
2015-09-25 19:59:21 +0200

    Incorporated architecture changes.

--- build_support/travis/setup_python.sh
+++ build_support/travis/setup_python.sh
@@ -17,15 +17,17 @@ if [ "${PYTHON_VERSION}" = "3" ]; then
   # chmod +x miniconda.sh
   # ./miniconda.sh -b
   sudo find / -name "libboost_python*.so" 2>/dev/null
-  sudo ln -s libboost_python-py34.so /usr/lib/x86_64-linux-gnu/libboost_python3.so
+  sudo ln -s libboost_python-py32.so /usr/lib/libboost_python3.so
   # export PYTHONPATH=/home/travis/miniconda3/lib/python3.4/site-packages
   # export PYTHON_INCLUDE_DIR=/home/travis/miniconda3/include/python3.4m
   # export PYTHON_LIBRARY=/home/travis/miniconda3/lib/libpython3.so
   # export MINICONDA_BIN=/home/travis/miniconda3/bin
-  sudo apt-get -y install python3 python3-dev python3-pip python3-numpy
-  sudo pip3 install --q scikit-learn
-  export PYTHON_INCLUDE_DIR=/usr/include/python3.4m
-  export PYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.4m.so
+  sudo apt-get -y install python3 python3-dev python3-numpy
+  wget https://bootstrap.pypa.io/get-pip.py
+  sudo python3 get-pip.py
+  sudo pip install -q scikit-learn
+  export PYTHON_INCLUDE_DIR=/usr/include/python3.2
+  export PYTHON_LIBRARY=/usr/lib/libpython3.2mu.so
   export PYTHON_EXECUTABLE=/usr/bin/python3
 fi
 # if [ ! "${PYTHON_VERSION}" = "none" ]; then
