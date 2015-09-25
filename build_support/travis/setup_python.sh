992cb32f110ecf0cf4e3149f72520770d8b6e54a
Christoph Lassner <mail@christophlassner.de>
2015-09-25 15:50:33 +0200

    Updated python setup file.

--- build_support/travis/setup_python.sh
+++ build_support/travis/setup_python.sh
@@ -4,16 +4,16 @@ if [ "${PYTHON_VERSION}" = "2" ]; then
   chmod +x miniconda.sh
   ./miniconda.sh -b
   export PYTHONPATH=/home/travis/miniconda/lib/python2.7/site-packages
-  export PYTHON_INCLUDE_DIR=/home/travis/miniconda/include/python3.4m
-  export PYTHON_LIBRARY=/home/travis/miniconda/lib/libpython3.so
+  export PYTHON_INCLUDE_DIR=/home/travis/miniconda/include/python2.7
+  export PYTHON_LIBRARY=/home/travis/miniconda/lib/libpython2.7.so
 fi
 if [ "${PYTHON_VERSION}" = "3" ]; then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
   chmod +x miniconda.sh
   ./miniconda.sh -b
   export PYTHONPATH=/home/travis/miniconda/lib/python3.4/site-packages
-  export PYTHON_INCLUDE_DIR=/home/travis/miniconda/include/python2.7
-  export PYTHON_LIBRARY=/home/travis/miniconda/lib/libpython2.7.so
+  export PYTHON_INCLUDE_DIR=/home/travis/miniconda/include/python3.4m
+  export PYTHON_LIBRARY=/home/travis/miniconda/lib/libpython3.so
 fi
 if [ ! "${PYTHON_VERSION}" = "none" ]; then
   export MINICONDA_BIN=/home/travis/miniconda/bin
