84a90499c6c56e8b7c599b3d54c3dd8f0fdb2659
Christoph Lassner <mail@christophlassner.de>
2015-09-25 18:01:45 +0200

    Python3 build change.

--- build_support/travis/setup_python.sh
+++ build_support/travis/setup_python.sh
@@ -12,7 +12,7 @@ if [ "${PYTHON_VERSION}" = "3" ]; then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
   chmod +x miniconda.sh
   ./miniconda.sh -b
-  set CWD_TMP=`pwd
+  set CWD_TMP=`pwd`
   cd /usr/lib/x86_64-linux-gnu
   sudo ln -s libboost_python-py34.so libboost_python3.so
   cd ${CWD_TMP}
